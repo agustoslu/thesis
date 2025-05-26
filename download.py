1e09f75281f3785903963f6b1c1c6bb6ea06f92c
agustoslu <tanalpagustoslu@gmail.com>
2025-05-26 21:30:15 +0200

    initial commit

---
+++ download.py
@@ -0,0 +1,35 @@
+# TODO: run ruff and mypy / typing 
+
+import os
+import requests
+import zipfile
+#from pyaml_env import parse_config
+from pathlib import Path
+
+#config = parse_config("config.yaml")
+home_dir = Path(__file__).parent
+DATASET_PATH = home_dir / "dataset"
+dataset_url = "https://physionet.org/static/published-projects/mimiciii-demo/mimic-iii-clinical-database-demo-1.4.zip" # use demo dataset for now, only includes 100 patients and clinical notes removed, 26 tables
+
+def download_file(url, save_path):
+    print(f"Downloading MIMIC-III Demo from {url}...")
+    response = requests.get(url, stream=True)
+    if response.status_code == 200:
+        with open(save_path, "wb") as file:
+            for chunk in response.iter_content(chunk_size=8192):
+                file.write(chunk)
+        print(f"Dataset saved to {save_path}.")
+    else:
+        raise Exception(f"Failed. Status code: {response.status_code}")
+
+def extract_zip(zip_path, extraction_path):
+    print(f"Extracting {zip_path} to {extraction_path}...")
+    with zipfile.ZipFile(zip_path, 'r') as zip_ref:
+        zip_ref.extractall(extraction_path)
+    print(f"Completed!")
+
+if __name__ == "__main__":
+    os.makedirs(DATASET_PATH, exist_ok=True)
+    ZIP_PATH = os.path.join(DATASET_PATH, "dataset.zip")
+    download_file(dataset_url, ZIP_PATH)
+    extract_zip(ZIP_PATH, DATASET_PATH)
