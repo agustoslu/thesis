2eb3f72f3157ab55231add32d788c6ef617de587
agustoslu <tanalpagustoslu@gmail.com>
2025-07-03 11:32:11 +0200

    add gru

--- dpnlp_lib/src/utils.py
+++ dpnlp_lib/src/utils.py
@@ -1,4 +1,8 @@
 import logging
+import torch
+import torch.nn as nn
+
+# TODO: some functions for plotting, incremental checkpointing, saving log outputs to respective folders, hardware monitoring, communication rounds
 
 
 def enable_info_logs() -> None:
@@ -12,4 +16,23 @@ def enable_info_logs() -> None:
 enable_info_logs()
 logger = logging.getLogger(__name__)
 
-# TODO: some functions for plotting, incremental checkpointing, saving log outputs to respective folders, hardware monitoring, communication rounds
+
+class MIMICGRU(nn.Module):
+    def __init__(self, input_dim, hidden_dim=128, num_layers=6, dropout=0.3):
+        super(MIMICGRU, self).__init__()
+        self.gru = nn.GRU(
+            input_size=input_dim,
+            hidden_size=hidden_dim,
+            num_layers=num_layers,
+            dropout=dropout if num_layers > 1 else 0.0,
+            batch_first=True,
+        )
+        self.hidden_size = hidden_dim
+
+    def forward(self, padded_sequences, lengths):
+        packed_sequences = nn.utils.rnn.pack_padded_sequence(
+            padded_sequences, lengths, batch_first=True, enforce_sorted=False
+        )
+        packed_output, hidden = self.gru(packed_sequences)
+        output, _ = nn.utils.rnn.pad_packed_sequence(packed_output, batch_first=True)
+        return output, hidden
