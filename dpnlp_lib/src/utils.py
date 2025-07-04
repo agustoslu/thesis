4b582ad8758a735c237cc86c98ef67f190f58ef8
agustoslu <tanalpagustoslu@gmail.com>
2025-07-04 16:06:12 +0200

    updates utils with flower helpers

--- dpnlp_lib/src/utils.py
+++ dpnlp_lib/src/utils.py
@@ -1,6 +1,11 @@
 import logging
 import torch
 import torch.nn as nn
+from flwr.common import NDArray
+from typing import Any, List, Tuple, Dict
+from torch.nn.utils.rnn import pad_sequence
+from collections import OrderedDict
+
 
 # TODO: some functions for plotting, incremental checkpointing, saving log outputs to respective folders, hardware monitoring, communication rounds
 
@@ -15,6 +20,39 @@ def enable_info_logs() -> None:
 
 enable_info_logs()
 logger = logging.getLogger(__name__)
+
+
+def collate_fn(
+    batch: List[Tuple[Any, Any]],
+) -> Tuple[torch.Tensor, List[int], torch.Tensor]:
+    features, labels = zip(*batch)
+    lengths = [f.shape[0] for f in features]
+    padded_features = pad_sequence(features, batch_first=True, padding_value=0.0)
+    labels = torch.stack(labels, dim=0)
+    return padded_features, lengths, labels
+
+
+def get_weights(model: nn.Module) -> list[NDArray]:
+    # from: https://github.com/matturche/flower_opacus_example/blob/main/flower_helpers.py#L133
+    return [value.cpu().numpy() for _, value in model.state_dict().items()]
+
+
+def set_weights(model: nn.Module, weights: list[NDArray]) -> None:
+    # from: https://github.com/matturche/flower_opacus_example/blob/main/flower_helpers.py#L138
+    params_dict = zip(model.state_dict().keys(), weights)
+    state_dict = OrderedDict({k: torch.Tensor(v) for k, v in params_dict})
+    model.load_state_dict(state_dict, strict=True)
+
+
+def get_target_delta(data_size: int) -> float:
+    # from: https://github.com/matturche/flower_opacus_example/blob/main/flower_helpers.py#L143
+    """Generate target delta δ given the size of a dataset. Delta should be
+    less than the inverse of the datasize. It is used as a fallback for epsilon ε, where the privacy is
+    not strictly preserved."""
+    denominator = 1
+    while data_size // denominator >= 1:
+        denominator *= 10
+    return 1 / denominator
 
 
 class MIMICGRU(nn.Module):
