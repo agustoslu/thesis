e404d73ab31b7a321592cb8bfe317e9d2af79414
agustoslu <tanalpagustoslu@gmail.com>
2025-07-06 16:25:02 +0200

    modify the server

--- dpnlp_lib/src/server.py
+++ dpnlp_lib/src/server.py
@@ -1,45 +1,31 @@
 import torch
 from typing import Any
-from flwr.server import ServerApp, ServerAppComponents
-from flwr.common import Context, ndarrays_to_parameters
-from flwr.common.typing import NDArray
-from flwr.server.strategy import FedAvg
-from utils import (
-    load_config,
-    load_model,
-    get_weights,
-    freeze_layers,
-)
-
-
-def setup_fedavg_strategy(model: torch.nn.Module, fraction_fit: float) -> FedAvg:
-    initial_weights: list[NDArray] = get_weights(model)
-    initial_parameters = ndarrays_to_parameters(initial_weights)
-
-    strategy = FedAvg(
-        fraction_fit=fraction_fit,
-        fraction_evaluate=1.0,
-        initial_parameters=initial_parameters,
-    )
-    return strategy
+from flwr.server import ServerApp, ServerAppComponents, ServerConfig
+from flwr.simulation import run_simulation
+from flwr.common import Context
+from dp_sgd import DPSGDFedAvg
+from dp_ftrl import DPFTRLFedAvg
 
 
 def server_fn(context: Context) -> ServerAppComponents:
-    config: dict[str, Any] = context.fl_config
-    num_rounds: int = config["num_server_rounds"]
-    fraction_fit: float = config["fraction_fit"]
+    config = ServerConfig(
+        num_rounds=200
+    )  # TODO: we might not explore this hyperparameter due to time constraints, still better to switch to config when they are all ready
+    return ServerAppComponents(
+        config=config, strategy=DPSGDFedAvg()
+    )  # pass config with conditional logic to use DPFTRLFedAvg
 
-    model_id: str = config["model_id"]
-    model: torch.nn.Module = load_model(model_id, load_config(model_id))
-    model = freeze_layers(model, config["use_lora"])
 
-    strategy: FedAvg = setup_fedavg_strategy(model, fraction_fit)
+server = ServerApp(server_fn=server_fn)
 
-    server_config = ServerAppComponents(
-        strategy=strategy, config={"num_rounds": num_rounds}
+backend_config = {"client_resources": None}
+if torch.cuda.is_available():
+    backend_config = {"client_resources": {"num_gpus": 1}}
+
+
+def run_flower_server(**kwargs: Any) -> None:
+    run_simulation(
+        server_app=server,
+        client_app=client,
+        **kwargs,
     )
-
-    return server_config
-
-
-app = ServerApp(server_fn=server_fn)
